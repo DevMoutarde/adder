39e2f2b1a1bfb58c46a62638c61d7ad1d9d6fe2f
GabinFOURCAULT <fourcault.gabin@gmail.com>
2022-08-05 14:37:40 +0200

    add divide

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-
-pub mod mathlib
-{
+pub mod mathlib {
     /// Ajoute 1 au nombre passé
     ///
     /// # exemple
@@ -10,30 +8,43 @@ pub mod mathlib
     /// assert_eq!(ajouter_un(3), 4);
     /// ```
     ///
-    pub fn ajouter_un(x: i32) -> i32
-    {
+    pub fn ajouter_un(x: i32) -> i32 {
         x + 1
+    }
+
+    /// division de x et y
+    ///
+    /// # exemple
+    ///
+    /// ```
+    /// use gabadder::mathlib::diviser;
+    /// assert_eq!(diviser(3.0, 2.0), 1.5);
+    /// ```
+    ///
+    pub fn diviser(x: f64, y: f64) -> f64 {
+        x / y
     }
 
     /// Retranche 1 au nombre passé
     ///
 
     ///
-    fn retrancher_un(x: i32) -> i32
-    {
-        x -1
+    fn retrancher_un(x: i32) -> i32 {
+        x - 1
     }
 }
 
-
-
 #[cfg(test)]
 mod tests {
-    use crate::mathlib::ajouter_un;
+    use crate::mathlib::{ajouter_un, diviser};
 
     #[test]
     fn it_works() {
-
         assert_eq!(ajouter_un(3), 4);
+    }
+
+    #[test]
+    fn it_divide() {
+        assert_eq!(diviser(3.0, 2.0), 1.5);
     }
 }
